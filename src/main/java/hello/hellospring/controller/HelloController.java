1bdbea3e608393e20b88dd9095cae378db44d3e8
sungjin555555 <103707815+sungjin555555@users.noreply.github.com>
2024-01-31 19:16:48 +0900

    comment

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -21,13 +21,13 @@ public class HelloController {
         return "hello-template";
     }
 
-    @GetMapping("hello-string")
+    @GetMapping("hello-string") // GetMapping
     @ResponseBody
     public String helloString(@RequestParam("name") String name){
         return "hello !! " + name;
     }
 
-    @GetMapping("hello-api")
+    @GetMapping("hello-api") // GetMapping
     @ResponseBody
     public Hello helloApi(@RequestParam("name") String name){
         Hello hello = new Hello();
