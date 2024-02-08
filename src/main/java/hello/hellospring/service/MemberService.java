770553e12976dfa44b840629f41026faee391a35
sungjin555555 <103707815+sungjin555555@users.noreply.github.com>
2024-02-08 22:52:10 +0900

    main

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -36,7 +36,7 @@ public class MemberService {
 
     private void validateDuplicateMember(Member member) {
         memberRepository.findByName(member.getName()).ifPresent(m -> {
-            throw new IllegalStateException("이미 존재하는 회원입니다!!!!!!");
+            throw new IllegalStateException("이미 존재하는 회원입니다!!!!!");
         });
     }
 
