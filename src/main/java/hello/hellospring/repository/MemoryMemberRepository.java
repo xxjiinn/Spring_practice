8076ad592674772da5faf5d1ff77645248f8f0b7
sungjin555555 <103707815+sungjin555555@users.noreply.github.com>
2024-02-10 00:48:54 +0900

    동시성 문제 고려

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -7,6 +7,7 @@ public class MemoryMemberRepository implements MemberRepository {
 
     /**
      * 동시성 문제가 고려되어 있지 않음, 실무에서는 ConcurrentHashMap, AtomicLong 사용 고려
+     * 그렇다고 합니다.
      */
     private static Map<Long, Member> store = new HashMap<>();
     private static long sequence = 0L;
